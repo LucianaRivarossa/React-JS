266be0b4e0447f692b82cb67ed679cd7cf083c3a
LucianaRivarossa <lucianarivarossa@gmail.com>
2022-10-26 23:16:55 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const saludo= () => {
+    console.log("hola")
+
+  }
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button className='boton' onClick={saludo}>
+        CLICK ME
+       </button>
+       <p className="parrafo">hola como estas todo eeeeeeee ? </p>
       </header>
     </div>
   );
